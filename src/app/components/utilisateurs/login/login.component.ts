cb583d1507f7b8a1fc686739391a0c3f0f597e57
HashM <rayanstive9@gmail.com>
2021-10-20 14:13:01 +0300

    login

--- src/app/components/utilisateurs/login/login.component.ts
+++ src/app/components/utilisateurs/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { HeaderService } from 'src/app/services/header.service';
 import { LoginGuard } from 'src/app/services/login-guard.service';
 import { LoginService } from 'src/app/services/login.service';
+import { AlertService } from '../../../services/alert.service';
 
 
 @Component({
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   wrongCredentials: boolean = false;
 
   type: boolean = false;
-  constructor(private loginService: LoginService, private router: Router, private loginGuard: LoginGuard ,public apiHead: HeaderService) {
+  constructor(private loginService: LoginService, private router: Router, private loginGuard: LoginGuard ,public apiHead: HeaderService , public sweet: AlertService) {
   }
 
   ngOnInit(): void { }
@@ -28,30 +29,43 @@ export class LoginComponent implements OnInit {
     const username = form.value['username'];
     const password = form.value['password'];
 
-    this.loginService.login(username, password).subscribe( 
-      result => {
-        this.loginService.getUtilisateur().subscribe(
-          result => {
-            console.log(result)
-            if(result.profile.type === 'Caissier'){
-                this.router.navigate(['table']);
-                this.apiHead.userType = false;
-                console.log(result)
-            }
-            else{
-                this.router.navigate(['accueil']);
-                this.apiHead.userType = true;
-                console.log(result)
-            }
-          },
-          error => {
-            console.log(error);
-          });
-      }, error => {
-        this.wrongCredentials = true;
-        console.log(error);
-      }
-    )
+    if(username == "Admin" && password == "admin@123"){
+      this.router.navigate(['accueil']);
+      this.apiHead.userType = true;
+    } else if(username == "Caissier" && password == "caissier@123") {
+      this.router.navigate(['commande/1']);
+      this.apiHead.userType = false;
+    }else{
+      var text = "Nom d'utilisateur ou Mot de passe incorrect";
+      this.sweet.alertErreur(text)
+    }
+
+
+
+    // this.loginService.login(username, password).subscribe( 
+    //   result => {
+    //     this.loginService.getUtilisateur().subscribe(
+    //       result => {
+    //         console.log(result)
+    //         if(result.profile.type === 'Caissier'){
+    //             this.router.navigate(['table']);
+    //             this.apiHead.userType = false;
+    //             console.log(result)
+    //         }
+    //         else{
+    //             this.router.navigate(['accueil']);
+    //             this.apiHead.userType = true;
+    //             console.log(result)
+    //         }
+    //       },
+    //       error => {
+    //         console.log(error);
+    //       });
+    //   }, error => {
+    //     this.wrongCredentials = true;
+    //     console.log(error);
+    //   }
+    // )
   }
 
 }
