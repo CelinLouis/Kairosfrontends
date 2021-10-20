29d2e45de56c713e9df8649540f8e9eb4c5700df
HashM <rayanstive9@gmail.com>
2021-10-20 11:04:20 +0300

    Build projet

--- serveur.js
+++ serveur.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/front'));
+app.use(express.static(__dirname + '/dist/KairosFrontend'));
 
 app.get('/*', function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/front/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/KairosFrontend/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
