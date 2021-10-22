d0d4d819eb1ab2491cdc8cce17b9ebd93047dcdc
HashM <rayanstive9@gmail.com>
2021-10-22 13:47:37 +0300

    Base url environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
 
   production: false,
-  DATABASE_URL: 'http://localhost:8000',
-  // DATABASE_URL: 'https://kairosbackends.herokuapp.com',
+  // DATABASE_URL: 'http://localhost:8000',
+  DATABASE_URL: 'https://kairosbackends.herokuapp.com',
 
   firebase: {
     apiKey: "AIzaSyCULoRMdRmgEd-LxVmmOpsp4WP7iZLGNXA",
